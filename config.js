aa7a64c24c49e4bef3b465d31f25ddc950ed319b
hidingskark <akangahmettle@gmail.com>
2024-12-28 07:39:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ SESSION_ID: process.env.SESSION_ID || "eJAw2DhD#OpkmKQQM2jmEW59KZWcyzVp7OIYOsKBH
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
 OWNER_NUMBER: process.env.OWNER_NUMBER || "233539339100",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/cLL0MWyT/JINHUYK-MD-V2.jpg", // DO NOT CHANGE
@@ -40,7 +40,7 @@ ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".", // .,!/?
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-HEART_REACT: process.env.HEART_REACT || "true",
+HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "『 ＪＩＮＨＵＹＫ-ＭＤ-Ｖ２ 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
